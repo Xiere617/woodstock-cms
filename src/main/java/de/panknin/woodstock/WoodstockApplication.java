2a0ef08f5f21c7d3e2ff9dfd977e0143f744e086
Maik Panknin <maik.panknin@arvato.com>
2020-01-31 21:04:29 +0100

    Initial commit

---
+++ src/main/java/de/panknin/woodstock/WoodstockApplication.java
@@ -0,0 +1,13 @@
+package de.panknin.woodstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodstockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodstockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/panknin/woodstock/WoodstockApplicationTests.java
@@ -0,0 +1,13 @@
+package de.panknin.woodstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
